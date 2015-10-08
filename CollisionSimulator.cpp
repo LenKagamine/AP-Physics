70e78ad40033e07b68377c549ee39b5dd7cf44dc
LenKagamine <micklepickle98@gmail.com>
2015-10-08 13:51:59 -0400

    Fix and update
    No more NaN, change to 2 balls, add I/O overloads, add comments

--- CollisionSimulator.cpp
+++ CollisionSimulator.cpp
@@ -1,34 +1,29 @@
-//
-//  main.cpp
-//  Collision Simulator
-
 #include <iostream>
 #include <vector>
 #include <cmath>
-
 using namespace std;
 
-struct force{
+class force{ //Force
+public:
     double x, y;
-    
     force(double x, double y){
         this->x = x;
         this->y = y;
     }
 };
 
-class ball {
+class ball{ //Ball
 public:
     double sx, sy;
     double vx, vy;
     double m; // = 57.2; // Yellow ball is 57.2 +- .1
     double k;
     double r;
-    
-    ball(){
+
+    ball(){ //Default constructor
         sx = sy = vx = vy = m = k = r = 0;
     }
-    ball(int sx, int sy, int vx, int vy, int m, int k, int r){
+    ball(double sx, double sy, double vx, double vy, double m, double k, double r){ //Constructor
         this->sx = sx;
         this->sy = sy;
         this->vx = vx;
@@ -37,75 +32,64 @@ public:
         this->k = k;
         this->r = r;
     }
-    
-    void applyForce(double fx, double fy, double dt){
+
+    void applyForce(double fx, double fy, double dt){ //Apply force to ball
         double ax = fx / m;
         double ay = fy / m;
-        
         vx += ax*dt;
         vy += ay*dt;
-        
         sx += vx*dt;
         sy += vy*dt;
     }
-    
-    void print(){
-        cout<<"s = ("<<sx<<","<<sy<<")\n";
-        cout<<"v = ("<<vx<<","<<vy<<")\n";
-    }
 };
 
-bool contin (vector<ball> balls, double t){
+istream &operator >>(istream &input, ball &b){ //Input
+	input >> b.sx >> b.sy >> b.vx >> b.vy >> b.m >> b.k >> b.r;
+	return input;
+}
+ostream &operator <<(ostream &output, ball &b){ //Output
+    output << "s = (" << b.sx << "," << b.sy <<")\n";
+    output << "v = (" << b.vx << "," << b.vy <<")\n";
+	return output;
+}
+
+bool running(ball b1, ball b2, double t){ //Whether to keep on running simulation
     return t < 4;
 }
 
-force getForce(ball b1, ball b2){
-    // calculate distance
+force getForce(ball b1, ball b2){ //Get force from collision
     double dx = b1.sx - b2.sx;
     double dy = b1.sy - b2.sy;
-    
     double d = sqrt(pow(dx,2) + pow(dy,2));
-    
     double comp = b1.r + b2.r - d;
-    
-    if (comp <= 0) return force(0,0);
-    
+    if(comp <= 0) return force(0,0);
+
     double fmag = b1.k*comp;
-    
-    force f = force(fmag*dx/d,fmag*dy/d);
-    
-    return f;
+    return force(fmag*dx/d,fmag*dy/d);
 }
 
-
-// ***** Main Method *****
-int main(int argc, const char *argv[]) {
-    vector<ball> balls (2);
-    
-    for(ball b: balls){
-        cout<<"enter ball values";
-        cin >> b.sx >> b.sy >> b.vx >> b.vy >> b.m >> b.k >> b.r;
-    }
-    
+int main(){
     double t = 0;
     double dt = 0.05;
-    
-    while (contin(balls, t)){
-        force f = getForce(balls[0],balls[1]);
-        
-        balls[0].applyForce(f.x,f.y,dt);
-        balls[1].applyForce(-f.x,-f.y,dt);
-        
-        cout<<"t = "<<t<<"\n";
-        cout<<"Ball 1:\n";
-        balls[0].print();
-        cout<<"Ball 2:\n";
-        balls[1].print();
-        cout<<endl;
-        
+    ball b1, b2;
+
+    cout<<"Enter ball 1:\n";
+    cin >> b1;
+    cout<<"Enter ball 2:\n";
+    cin >> b2;
+
+    while(running(b1, b2, t)){
+        force f = getForce(b1,b2);
+
+        b1.applyForce(f.x,f.y,dt);
+        b2.applyForce(-f.x,-f.y,dt);
+
+        cout << "t = " << t << ":\n";
+        cout << "Ball 1:\n" << b1;
+        cout << "Ball 2:\n" << b2;
+        cout << endl;
+
         t += dt;
     }
-    
     return 0;
 }
-
