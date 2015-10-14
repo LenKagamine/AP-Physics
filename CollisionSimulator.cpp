088e77b5dad5423b279041343f4e914dbd14709c
LenKagamine <micklepickle98@gmail.com>
2015-10-13 20:43:50 -0400

    Remove multiple balls, hardcode k-value
    Return back to 2 balls only; k-value hard coded as stress ball

--- CollisionSimulator.cpp
+++ CollisionSimulator.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
 #include <fstream>
 
@@ -18,47 +17,37 @@ class ball{ //Ball
 public:
     double sx, sy; //Translational motion
     double vx, vy;
-    double ax, ay;
-    double omega, alpha; //Rotational motion
+    double omega; //Rotational motion
     double m; // Yellow ball is 57.2 +- .1
-    double k;
     double r;
     double inertia;
 
     ball(){ //Default constructor
-        sx = sy = vx = vy = m = k = r = omega = alpha = inertia = 0;
+        sx = sy = vx = vy = m = r = omega = inertia = 0;
     }
-    ball(double sx, double sy, double vx, double vy, double m, double k, double r){ //Constructor
+    ball(double sx, double sy, double vx, double vy, double m){ //Constructor
         this->sx = sx;
         this->sy = sy;
         this->vx = vx;
         this->vy = vy;
-        ax = ay = 0;
-    	omega = alpha = 0;
+    	omega = 0;
         this->m = m;
-        this->k = k;
         this->r = r;
         inertia = 2*m*r*r/5;
     }
 
-    void addForce(force f){ //Apply force
-    	ax += f.x / m;
-    	ay += f.y / m;
+    void applyForce(double fx, double fy, double dt){ //Apply force
+    	double ax = fx / m;
+    	double ay = fy / m;
+        vx += ax * dt;
+        vy += ay * dt;
+        sx += vx * dt;
+        sy += vy * dt;
     }
 
-    void applyTorque(double f, double theta){ //Apply torque
-        alpha = f * r * sin(theta) / inertia;
-    }
-
-    void update(double dt){ //Update ball with forces
-        vx += ax*dt;
-        vy += ay*dt;
-        sx += vx*dt;
-        sy += vy*dt;
-        ax = ay = 0;
-
-        omega += alpha*dt;
-        alpha = 0;
+    void applyTorque(double force, double angle, double dt){ //Apply torque
+        double alpha = force * r * sin(angle) / inertia;
+        omega += alpha * dt;
     }
 };
 istream &operator >>(istream &input, ball &b){ //Input
@@ -72,8 +61,6 @@ istream &operator >>(istream &input, ball &b){ //Input
     input >> b.vy;
     cout << "m: ";
     input >> b.m;
-    cout << "k: ";
-    input >> b.k;
     cout << "r: ";
     input >> b.r;
 	return input;
@@ -90,58 +77,39 @@ force getForce(ball b1, ball b2){ //Get force from collision
     double comp = b1.r + b2.r - d;
     if(comp <= 0) return force(0,0);
 
-    double fmag = b1.k*comp;
+    double fmag = 85000 * comp * comp + 410 * comp; //from stress ball
     return force(fmag*dx/d,fmag*dy/d);
 }
 
-bool running(vector<ball> balls, double t){ //Whether to keep on running simulation
+bool running(ball b1, ball b2, double t){ //Whether to keep on running simulation
     return t < 4;
 }
 
 int main(){
-    double dt = 0.0001;
-    int num;
-    cout << "Number of balls: ";
-    cin >> num;
-    vector<ball> balls(num);
-    for(int i=0; i<num; ++i){
-      	cout << "Enter ball " << i+1 << ":\n";
-      	cin >> balls[i];
+    double dt = 0.0001; //Timestep
+    ball b1, b2; //Two balls
+    cout << "Enter ball 1:\n";
+    cin >> b1;
+    cout << "Enter ball 2:\n";
+    cin >> b2;
+
+    ofstream output("data.csv"); //Create output data file
+    if(!output) cout << "Output file not created."; //Warning if data file couldn't be created
+    output << "t,sx1,sy1,vx1,vy1,sx2,sy2,vx2,vy2\n"; //Column titles
+
+    for(double t=0; running(b1, b2, t); t+=dt){
+        force f = getForce(b1, b2);
+
+        b1.applyForce(f.x, f.y, dt);
+        b2.applyForce(-f.x, -f.y, dt);
+
+        output << t << "," << b1 << "," << b2 << endl;
     }
 
-	ofstream output("data.csv");
-	if(!output) cout << "Output file notcreated.";
-	output << "t";
-	for(int i=1; i<=num; ++i){
-        output << ",sx" << i;
-        output << ",sy" << i;
-        output << ",vx" << i;
-        output << ",vy" << i;
-	}
-	output << endl;
-
-	for(double t=0; running(balls, t); t+=dt){
-        for(int i=0; i<num; ++i){
-            for(int j=0; j<num; ++j){
-                if(i != j){
-                    force f = getForce(balls[i], balls[j]);
-                    balls[i].addForce(f);
-                }
-            }
-        }
-
-		output << t;
-		for(int i=0; i<num; ++i){
-            balls[i].update(dt);
-        	output << "," << balls[i];
-		}
-		output << endl;
-    }
+    output.close();
 
     cout << "Simulation complete.\nFinal values:\n";
-    for(int i=0; i<num; ++i){
-        cout << i+1 << ": " << balls[i] << endl;
-    }
-    output.close();
+    cout << "Ball 1: " << b1 << endl;
+    cout << "Ball 2: " << b2 << endl;
     return 0;
 }
