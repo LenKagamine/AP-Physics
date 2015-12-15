163cfad51c15a3e1c42c976abe7c6e66cef12387
LenKagamine <micklepickle98@gmail.com>
2015-12-15 16:12:23 -0500

    Fix solenoid sim

--- SolenoidSimulator.cpp
+++ SolenoidSimulator.cpp
@@ -8,54 +8,6 @@ double R = 0.07; //y coefficient
 double f = 320 / 0.022; //frequency
 double w = 2 * 3.141592 * f; //angular frequency
 
-double x(double t) {
-    return R * cos(w * t) + 0.10;
-}
-
-double y(double t) {
-    return R * sin(w * t) + 0.10;
-}
-
-double z(double t) {
-    return t + 0.03;
-}
-
-double xp(double t) {
-    return -w * R * sin(w * t);
-}
-
-double yp(double t) {
-    return w * R * cos(w * t);
-}
-
-double zp(double t) {
-    return 1;
-}
-
-double x2(double t) {
-    return -t + 0.192;
-}
-
-double y2(double t) {
-    return R * sin(w * t) + 0.10;
-}
-
-double z2(double t) {
-    return R * cos(w * t) + 0.10;
-}
-
-double x2p(double t) {
-    return -1;
-}
-
-double y2p(double t) {
-    return w * R * cos(w * t);
-}
-
-double z2p(double t) {
-    return -w * R * sin(w * t);
-}
-
 class Vector3D{
 public:
     double x, y, z;
@@ -82,6 +34,35 @@ public:
         return sqrt(x * x + y * y + z * z);
     }
 };
+
+Vector3D pos(double t){
+    double x = R * cos(w * t) + 0.10;
+    double y = R * sin(w * t) + 0.10;
+    double z = t + 0.03;
+    return Vector3D(x, y, z);
+}
+
+Vector3D dpos(double t){
+    double xp = -w * R * sin(w * t);
+    double yp = w * R * cos(w * t);
+    double zp = 1;
+    return Vector3D(xp, yp, zp);
+}
+
+Vector3D pos2(double t){
+    double x = -t + 0.192;
+    double y = R * sin(w * t) + 0.10;
+    double z = R * cos(w * t) + 0.10;
+    return Vector3D(x, y, z);
+}
+
+Vector3D dpos2(double t){
+    double xp = -1;
+    double yp = w * R * cos(w * t);
+    double zp = -w * R * sin(w * t);
+    return Vector3D(xp, yp, zp);
+}
+
 int main(){
     double mu = 0.00000125663;  //vacuum permeability
     double pi = 3.141592;       //pi
@@ -90,6 +71,7 @@ int main(){
 
     double Tstart = 0;          //starting t
     double Tend = 0.022;        //ending t
+    double t = Tstart;          //t for integrating
     int sizex = 24;             //length in x (cm)
     int sizey = 24;             //length in y (cm)
     int sizez = 24;             //length in z (cm)
@@ -120,13 +102,12 @@ int main(){
     Vector3D integral2; //integral (2nd solenoid)
 
     cout << "Starting simulation.\n";
-    for(double t = Tstart; t <= Tend; t += dt){ //integrating around the solenoids
+    while(Tstart <= t && t <= Tend){ //integrating around the solenoids
         //reusable calculations for faster simulation
-        l = Vector3D(x(t), y(t), z(t));
-        l2 = Vector3D(x2(t), y2(t), z2(t));
-
-        dl = Vector3D(xp(t), yp(t), zp(t)) * dt;
-        dl2 = Vector3D(x2p(t), y2p(t), z2p(t)) * dt;
+        l = pos(t);
+        l2 = pos2(t);
+        dl = dpos(t) * dt;
+        dl2 = dpos2(t) * dt;
 
         for(int fieldx = 0; fieldx < bfield.size(); fieldx++){ //x coordinate
             for(int fieldy = 0; fieldy < bfield[0].size(); fieldy++){ //y coordinate
@@ -137,14 +118,15 @@ int main(){
 
                     //second solenoid
                     rprime2 = Vector3D(fieldx, fieldy, fieldz) * sizeConst - l2;
-                    integral2 = dl2.cross(rprime) * (constant / pow(rprime.magnitude(), 3.0));
+                    integral2 = dl2.cross(rprime2) * (constant / pow(rprime2.magnitude(), 3.0));
 
                     //sum the integrands
                     bfield[fieldx][fieldy][fieldz] = bfield[fieldx][fieldy][fieldz] + integral + integral2;
                 }
             }
         }
-        //cout << t << endl;
+        cout << t << endl;
+        t += dt;
     }
 
     //output to csv
